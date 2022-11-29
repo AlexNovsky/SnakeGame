062d908305d2b0eb888cc3286478461e03c3ae42
Alex Novsky <68556113+AlexNovsky@users.noreply.github.com>
2022-11-29 07:28:14 -0600

    Adding database for highest score as a file to keep highest score for the next games.

--- scoreboard.py
+++ scoreboard.py
@@ -7,6 +7,7 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.hscore = 0
         self.high_score = 0
         self.color("green")
         self.penup()
@@ -14,13 +15,23 @@ class Score(Turtle):
         self.update_scoreboard()
         self.hideturtle()
 
+    def write_hscore(self):
+        with open("score_base.txt", "w") as file:
+            file.write(f"{self.high_score}")
+
+    def read_hscore(self):
+        with open("score_base.txt") as file:
+            self.hscore = file.read()
+
     def update_scoreboard(self):
         self.clear()
-        self.write(f"Score: {self.score}  Highest score: {self.high_score}", align=ALINGMENT, font=FONT)
+        self.read_hscore()
+        self.write(f"Score: {self.score}  Highest score: {self.hscore}", align=ALINGMENT, font=FONT)
 
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            self.write_hscore()
         self.score = 0
         self.clear()
         self.update_scoreboard()
